0354882aca08c6a1e24d97bc4180c6e70c7b4036
Jorge O. Castro <jorge.castro@gmail.com>
2025-01-04 16:41:38 -0500

    fix: update installation command

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -13,7 +10,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+dnf install -y tmux 
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
