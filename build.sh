acf114c41649ca45b17b84fe884747c63ba24979
Jorge O. Castro <jorge.castro@gmail.com>
2025-01-11 19:25:40 -0500

    feat: add copr example (#58)
    And remove duplicate rpm-ostree example.

--- build.sh
+++ build.sh
@@ -12,8 +12,12 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf install -y tmux 
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
