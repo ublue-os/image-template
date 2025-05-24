ublue-os/image-template

commit 083532b8ff8399eedf1f2fdad994420a1db595c1

fix(justfile): `just lint` and `just format` pass when `shellcheck` and `shfmt` do not exist Fixes #104

Justfile
@@ -325,8 +325,24 @@ spawn-vm rebuild="0" type="qcow2" ram="6G":
 
 # Runs shell check on all Bash scripts
 lint:
+    #!/usr/bin/env bash
+    set -eoux pipefail
+    # Check if shellcheck is installed
+    if ! command -v shellcheck &> /dev/null; then
+        echo "shellcheck could not be found. Please install it."
+        exit 1
+    fi
+    # Run shellcheck on all Bash scripts
     /usr/bin/find . -iname "*.sh" -type f -exec shellcheck "{}" ';'
 
 # Runs shfmt on all Bash scripts
 format:
+     #!/usr/bin/env bash
+    set -eoux pipefail
+    # Check if shellcheck is installed
+    if ! command -v shfmt &> /dev/null; then
+        echo "shellcheck could not be found. Please install it."
+        exit 1
+    fi
+    # Run shellcheck on all Bash scripts
     /usr/bin/find . -iname "*.sh" -type f -exec shfmt --write "{}" ';'

commit c59c2c8dd548bd1f1fb661daf7fceed44037cd0b

fix(just): fix syntax error
introduced in 083532b8ff

error: Recipe line has inconsistent leading whitespace. Recipe started with `␠␠␠␠␠` but found line with `␠␠␠␠`
——▶ Justfile:341:1
│
341 │     set -eoux pipefail
│ ^^^^

Justfile
@@ -337,7 +337,7 @@ lint:
 
 # Runs shfmt on all Bash scripts
 format:
-     #!/usr/bin/env bash
+    #!/usr/bin/env bash
     set -eoux pipefail
     # Check if shfmt is installed
     if ! command -v shfmt &> /dev/null; then