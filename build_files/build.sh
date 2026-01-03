713084d7c5dabf26b28ed11ff56d54fcf865a90c
yodatak <mryodatak@gmail.com>
2026-01-03 16:59:50 +0100

    Update build.sh (#182)

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,7 +7,7 @@ set -ouex pipefail
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
 dnf5 install -y tmux 
