7829a51a502a2d4fc4e5614d6c318368f654e679
cen3p <mazotikul@gmail.com>
2026-06-04 02:01:13 +0200

    Add host packages: neovim, tmux, git, tailscale, wireguard, kitty, awesome, etc.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,30 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+    neovim \
+    tmux \
+    git \
+    smartmontools \
+    nvme-cli \
+    tailscale \
+    wireguard-tools \
+    kitty \
+    awesome \
+    btop \
+    fastfetch \
+    ripgrep \
+    fd-find \
+    jq \
+    yq \
+    tree \
+    rsync \
+    rclone \
+    htop \
+    wget \
+    curl \
+    tshark \
+    wezterm
 
 # Use a COPR Example:
 #
