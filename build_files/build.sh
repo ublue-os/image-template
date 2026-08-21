5f4420042a30e54ff8da4013233052fd0213ba22
Charles Gardner <1515419+opsroller@users.noreply.github.com>
2026-08-21 01:10:36 -0600

    chore: initialize sovereignite-os skeleton from image-template

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,19 +8,18 @@ cp -avf "/ctx/system_files"/. /
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+# SovereigniteOS is a headless ucore-based image for K8s nodes, so we only
+# install server-basics utilities here (no desktop/Bazzite packages).
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
+# basic utilities
+dnf5 install -y tmux htop curl git
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# --- sovereignite packages ---
+# dnf5 -y install containerd kubernetes kubelet kubeadm
+
+# --- sovereignite services ---
+# systemctl enable kubelet
+# systemctl enable containerd
 
 #### Example for enabling a System Unit File
 
