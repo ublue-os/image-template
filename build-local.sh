b6c35e1c4f3636db5b55fe48d2f6cc12cdee9119
Duong Tran <tranduong1988@gmail.com>
2026-08-25 09:27:51 +0700

    update base_image for build-local

--- build-local.sh
+++ build-local.sh
@@ -1,3 +1,3 @@
-sudo podman build --build-arg BASE_IMAGE=ghcr.io/ublue-os/bazzite:stable -t pigeon-os . && \
+sudo podman build --build-arg BASE_IMAGE=quay.io/fedora/fedora-kinoite -t pigeon-os . && \
 sudo bootc switch --transport containers-storage localhost/pigeon-os:latest
 sudo reboot
